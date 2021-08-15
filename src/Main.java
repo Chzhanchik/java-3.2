1529e43061e56c2d8c42a8dfc3ea6cb5d4d6d191
chzhanchik <chzhanchik@gmail.com>
2021-08-15 23:10:55 +0300

    origin

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int balance = 100;
         int count = 100;
         int bonus = 1;
-        int transfer = 2000;
-        if (transfer >= 1000)
+        int transfer = 999;
+        if (transfer > 1000)
             System.out.println((transfer / count * bonus) + transfer + balance);
         else
         System.out.println(transfer + balance);
