a2faa37b5fc1ecfc565b09d4f004c5426e1daf26
chzhanchik <chzhanchik@gmail.com>
2021-08-15 22:55:19 +0300

    origin

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int count = 100;
+        int bonus = 1;
+        int transfer = 2000;
+        if (transfer >= 1000)
+            System.out.println((transfer / count * bonus) + transfer + balance);
+        else
+        System.out.println(transfer + balance);
+    }
+}
